5ac6fcc39a2abb3e76fd169d6d7372cb44d2c346
cj-rhee <cj.rhee@smartstudygames.com>
2021-06-25 14:45:22 +0900

    update editor

--- Packages/LocusBundleSystem/Editor/AssetBundleTrackingVisualizer.cs
+++ Packages/LocusBundleSystem/Editor/AssetBundleTrackingVisualizer.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEditor;
 using TrackInfo = BundleSystem.BundleManager.TrackInfo;
 using System.Collections.Generic;
@@ -14,13 +14,18 @@ public class AssetBundleTrackingVisualizer : EditorWindow
     void OnGUI()
     {
         BundleSystem.BundleManager.GetTrackingSnapshot(s_TrackInfoCache);
-        m_ScrollPosition = EditorGUILayout.BeginScrollView(m_ScrollPosition);
+        m_ScrollPosition = EditorGUILayout.BeginScrollView(m_ScrollPosition, false, true);
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField($"Key", GUILayout.Width(200));
+        EditorGUILayout.LabelField($"Owner", GUILayout.Width(200));
+        EditorGUILayout.LabelField($"Asset", GUILayout.Width(200));
+        EditorGUILayout.EndHorizontal();
         foreach(var kv in s_TrackInfoCache)
         {
             EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField($"{kv.Key} - {kv.Value.BundleName.ToString()}");
-            EditorGUILayout.ObjectField(new GUIContent("Owner"), kv.Value.Owner, typeof(UnityEngine.Object), true);
-            EditorGUILayout.ObjectField(new GUIContent("Asset"), kv.Value.Asset, typeof(UnityEngine.Object), false);
+            EditorGUILayout.LabelField($"{kv.Key} - {kv.Value.BundleName.ToString()}", GUILayout.Width(200));
+            EditorGUILayout.ObjectField(kv.Value.Owner, typeof(UnityEngine.Object), true, GUILayout.Width(200));
+            EditorGUILayout.ObjectField(kv.Value.Asset, typeof(UnityEngine.Object), false, GUILayout.Width(200));
             EditorGUILayout.EndHorizontal();
         }
         EditorGUILayout.EndScrollView();
